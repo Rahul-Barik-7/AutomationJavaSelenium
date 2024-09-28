e8929819c0c819bb8732e3e68d513d18f7bea121
91707 <rahulbarik481@gmail.com>
2024-09-28 15:38:47 +0530

    This is all about Synchronization

---
+++ com.LearningSelenium/src/test/java/com/Synchronization/ExplicitWait.java
@@ -0,0 +1,27 @@
+package com.Synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWait {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.swiggy.com/");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+				"//img[@src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_North Indian.png']")));
+		driver.findElement(By.xpath(
+				"//img[@src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_North Indian.png']"))
+				.click();
+	}
+
+}
---
+++ com.LearningSelenium/src/test/java/com/Synchronization/ImplicitWait.java
@@ -0,0 +1,20 @@
+package com.Synchronization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.youtube.com");
+
+		
+	}
+
+}
